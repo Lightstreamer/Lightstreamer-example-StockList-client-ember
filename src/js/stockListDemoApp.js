3a099307b550717805f450ddf3cb5e7b98c8adb8
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2015-01-22 16:37:56 +0100

    Improve indentation

--- src/js/stockListDemoApp.js
+++ src/js/stockListDemoApp.js
@@ -63,8 +63,8 @@ StockListDemoApp.StockRoute = Ember.Route.extend({
   activate: function() {
     store = this.store;
     require(["js/lsClient", "Subscription"], function(lsClient, Subscription) {
-      
-	  var stockSubscription = new Subscription("MERGE", 
+
+      var stockSubscription = new Subscription("MERGE", 
         ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"],
         ["stock_name", "last_price", "time", "pct_change", "bid_quantity", "bid", "ask", "ask_quantity", "min", "max", "ref_price", "open_price"]
       );
